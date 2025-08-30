49abb5534a9cee6526c31a8df278b93e5a7d848f
Max Kapur <git@maxkapur.com>
2025-08-30 16:31:11 -0500

    Copy tree or file conditionally

--- dated.py
+++ dated.py
@@ -7,6 +7,8 @@ from datetime import date
 from sys import stderr
 import shutil
 
+from pathlib import Path
+
 STARTS_WITH_TIMESTAMP = re.compile(r"(\d\d\d\d\-\d\d\-\d\d)_")
 
 
@@ -22,3 +24,26 @@ def with_current_date(filename: str, now: date = date.today()) -> str:
         raise ValueError(f"{filename} already begins with today's date!")
 
     return f"{nowstamp}_{rest_of_filename}"
+
+
+def copy_dated(infile: Path, now: date = date.today()) -> None:
+    if not infile.exists():
+        raise FileNotFoundError(f"{infile} doesn't exist")
+
+    dest = infile.parent / with_current_date(infile.name, now)
+    if infile.is_file():
+        stderr.write(f"copy2({infile}, {dest})")
+        shutil.copy2(infile, dest)
+    elif infile.is_dir():
+        stderr.write(f"copytree({infile}, {dest})")
+        shutil.copytree(infile, dest)
+    else:
+        raise ValueError(f"{infile} is neither a file nor a directory")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile")
+    parsed = parser.parse_args()
+    infile = Path(parsed.infile)
+    return copy_dated(infile)
