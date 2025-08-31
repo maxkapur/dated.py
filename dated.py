f38b90ff90d2e16c207ba3209464de900078bb84
Max Kapur <git@maxkapur.com>
2025-08-30 20:08:10 -0500

    infile -> inpath (could be file or dir)

--- dated.py
+++ dated.py
@@ -26,26 +26,26 @@ def with_current_date(filename: str, today: date = date.today()) -> str:
     return f"{nowstamp}_{rest_of_filename}"
 
 
-def copy_dated(infile: Path, today: date = date.today()) -> None:
-    if not infile.exists():
-        raise FileNotFoundError(f"{infile} doesn't exist")
+def copy_dated(inpath: Path, today: date = date.today()) -> None:
+    if not inpath.exists():
+        raise FileNotFoundError(f"{inpath} doesn't exist")
 
-    dest = infile.parent / with_current_date(infile.name, today)
-    if infile.is_file():
-        stderr.write(f"copy2({infile}, {dest})")
-        shutil.copy2(infile, dest)
-    elif infile.is_dir():
-        stderr.write(f"copytree({infile}, {dest})")
-        shutil.copytree(infile, dest)
+    dest = inpath.parent / with_current_date(inpath.name, today)
+    if inpath.is_file():
+        stderr.write(f"copy2({inpath}, {dest})")
+        shutil.copy2(inpath, dest)
+    elif inpath.is_dir():
+        stderr.write(f"copytree({inpath}, {dest})")
+        shutil.copytree(inpath, dest)
     else:
-        raise ValueError(f"{infile} is neither a file nor a directory")
+        raise ValueError(f"{inpath} is neither a file nor a directory")
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="Create a new copy of a file or directory with today's date as a prefix"
     )
-    parser.add_argument("infile", help="Input file/directory to dated")
+    parser.add_argument("inpath", help="Input file/directory to dated")
     parsed = parser.parse_args()
-    infile = Path(parsed.infile)
-    return copy_dated(infile)
+    inpath = Path(parsed.inpath)
+    return copy_dated(inpath)
