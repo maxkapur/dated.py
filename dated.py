68f4dd9a44cd91de3920dfebc090c8567c3ce465
Max Kapur <git@maxkapur.com>
2025-08-30 20:06:11 -0500

    now -> today

--- dated.py
+++ dated.py
@@ -12,25 +12,25 @@ from pathlib import Path
 STARTS_WITH_TIMESTAMP = re.compile(r"(\d\d\d\d\-\d\d\-\d\d)_")
 
 
-def with_current_date(filename: str, now: date = date.today()) -> str:
-    nowstamp = now.strftime(r"%Y-%m-%d")
+def with_current_date(filename: str, today: date = date.today()) -> str:
+    nowstamp = today.strftime(r"%Y-%m-%d")
     m = re.match(STARTS_WITH_TIMESTAMP, filename)
     if not m:
         return f"{nowstamp}_{filename}"
 
     rest_of_filename = filename[m.span()[1] :]
     then = date.fromisoformat(m.group(1))
-    if then == now:
+    if then == today:
         raise ValueError(f"{filename} already begins with today's date!")
 
     return f"{nowstamp}_{rest_of_filename}"
 
 
-def copy_dated(infile: Path, now: date = date.today()) -> None:
+def copy_dated(infile: Path, today: date = date.today()) -> None:
     if not infile.exists():
         raise FileNotFoundError(f"{infile} doesn't exist")
 
-    dest = infile.parent / with_current_date(infile.name, now)
+    dest = infile.parent / with_current_date(infile.name, today)
     if infile.is_file():
         stderr.write(f"copy2({infile}, {dest})")
         shutil.copy2(infile, dest)
